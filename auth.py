3dff5698c0791da82cedc0868e04fab016f8e0ef
stebil87 <97510620+stebil87@users.noreply.github.com>
2026-06-06 09:52:59 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -23,7 +23,6 @@ def check_auth():
     return st.session_state.utente
 
 def pagina_login():
-    st.title("CRM — Accesso")
     st.markdown("---")
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
