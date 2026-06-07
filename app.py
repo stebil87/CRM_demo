387381b609afe8e5eabe53c23db07de124570bd1
stebil87 <97510620+stebil87@users.noreply.github.com>
2026-06-07 08:56:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ section[data-testid="stSidebar"] .streamlit-expanderContent {
 }
 
 .main .block-container {
-    padding: 2rem 2.5rem;
+    padding: 0.5rem 2.5rem 2rem 2.5rem;
     max-width: 1400px;
 }
 
